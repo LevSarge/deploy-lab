b137588ad32841274167f1f6d7004078e21c4f4f
LevSarge <levimsargeant@gmail.com>
2022-01-02 21:04:05 -0700

    Changed up some things in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path")
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 5000
 
 const cors = require("cors");
 
